6b4ba0373f8d9236eb54e14ea9af245b41cd31c7
SimonPalmgren <simon.palmgren91@gmail.com>
2019-12-15 16:00:10 +0100

    50/50 function

--- quiz/src/components/Answers/Answers.js
+++ quiz/src/components/Answers/Answers.js
@@ -9,6 +9,25 @@ const Answers = (props) => {
   let answers = [...props.answer.answers]
   shuffleData(answers)
 
+
+  const fityFifty = () => {
+    let arr = []
+    let wrongAnswer = []
+    let spreadLifelineAnswers = [...answers]
+    let filteredCorrectAnswer = spreadLifelineAnswers.filter(correctAnswer => correctAnswer === props.answer.correct_answer)
+    let indexOfCorrectAnswer = answers.indexOf(props.answer.correct_answer)
+    spreadLifelineAnswers.splice(indexOfCorrectAnswer, 1)
+    var randomWrongAnswerIndex = Math.floor(Math.random() * spreadLifelineAnswers.length);
+    spreadLifelineAnswers.filter((wrongAnswerOfArray, index) => {
+      if (randomWrongAnswerIndex === index) {
+        wrongAnswer.push(wrongAnswerOfArray)
+      }
+    })
+    arr = [...filteredCorrectAnswer, ...wrongAnswer]
+    console.log(arr)
+  }
+  fityFifty()
+
   const listAnswers = answers.map((answer, index) =>
     <Answer
       key={index}
