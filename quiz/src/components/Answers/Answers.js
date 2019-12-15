d3e220457af140baa5d40c3a78d660f78f30d95f
SimonPalmgren <simon.palmgren91@gmail.com>
2019-12-15 16:50:23 +0100

    Moved to useEffect to only load once

--- quiz/src/components/Answers/Answers.js
+++ quiz/src/components/Answers/Answers.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Answer from '../Answer/Answer'
 import { shuffleData } from '../../helper/shuffle'
 import './answers-styles.scss'
 
 
 const Answers = (props) => {
-
+  const [rightAndWrong, setRightAndWrong] = useState([])
+  const [unClickAnsw, setUnClickAnsw] = useState([])
   let answers = [...props.answer.answers]
   shuffleData(answers)
+  console.log(rightAndWrong)
+  console.log(unClickAnsw)
 
-
-  const fityFifty = () => {
+  useEffect(() => {
     let arr = []
     let wrongAnswer = []
+    let unclickableAnswers = []
+    let getIndexOfWrongAnswerString
     let spreadLifelineAnswers = [...answers]
     let filteredCorrectAnswer = spreadLifelineAnswers.filter(correctAnswer => correctAnswer === props.answer.correct_answer)
     let indexOfCorrectAnswer = answers.indexOf(props.answer.correct_answer)
@@ -21,12 +25,18 @@ const Answers = (props) => {
     spreadLifelineAnswers.filter((wrongAnswerOfArray, index) => {
       if (randomWrongAnswerIndex === index) {
         wrongAnswer.push(wrongAnswerOfArray)
+        getIndexOfWrongAnswerString = wrongAnswerOfArray
       }
     })
+    let getIndexOfWrongAnswer = spreadLifelineAnswers.indexOf(getIndexOfWrongAnswerString)
+    spreadLifelineAnswers.splice(getIndexOfWrongAnswer, 1)
+
+    unclickableAnswers = [...spreadLifelineAnswers]
     arr = [...filteredCorrectAnswer, ...wrongAnswer]
-    console.log(arr)
-  }
-  fityFifty()
+    setRightAndWrong(arr)
+    setUnClickAnsw(unclickableAnswers)
+  }, [])
+
 
   const listAnswers = answers.map((answer, index) =>
     <Answer
